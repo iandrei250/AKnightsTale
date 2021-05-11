01d4bec3d1e496451525f177fc619893b5676662
iandrei250 <prtdace@gmail.com>
2021-05-11 15:26:20 +0200

    movement animations

--- A Knight's Tale/Assets/Scripts/Movement/MoveController.cs
+++ A Knight's Tale/Assets/Scripts/Movement/MoveController.cs
@@ -7,11 +7,20 @@ using UnityEngine.AI;
 
   public class MoveController : MonoBehaviour
   {
+
+    Ray lastRay;
+   float fixedRotation = 0;
+   
+
+
    private void Update() {
      if(Input.GetMouseButtonDown(0)){
        MoveTo();
      }
+
+     UpdateAnimator();
    }
+
 
   private void MoveTo()
   {
@@ -21,7 +30,17 @@ using UnityEngine.AI;
 
     if(hasHit){
       GetComponent<NavMeshAgent>().destination = hitInfo.point;
+      
     }
+  }
+  private void UpdateAnimator()
+  {
+    Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+    Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+    float speed = localVelocity.z;
+    
+    GetComponent<Animator>().SetFloat("fwdSpeed", speed);
   }
 }//end of class
 
